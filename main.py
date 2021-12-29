67b87c8febb7f0f0250b31b855d2c938e6678ad7
Philippe Mourey <philippe@mourey.com>
2021-12-29 20:58:35 +0100

    full monster database :-)

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-from typing import List
-
-import random
-
 import json
+import random
 from dataclasses import dataclass
+from typing import List
 
 import requests
 
@@ -13,6 +11,7 @@ class Character:
     name: str
     armor_class: int
     hit_points: int
+    xp: int
 
 
 @dataclass
@@ -22,10 +21,29 @@ class Monster:
     hit_points: int
     hit_dice: str
     xp: int
+    challenge_rating: int
 
     def attack(self):
+        """
+        :return: damage generated
+        """
         dice_count, roll_dice = map(int, self.hit_dice.split('d'))
         return sum([random.randint(1, roll_dice) for _ in range(dice_count)])
+
+
+def download_monster(index_name: str):
+    """
+    Downloads a monster's characteristic to a local .json file
+    :param index_name: name of the monster
+    :return:
+    """
+    # api-endpoint
+    url = f"https://www.dnd5eapi.co/api/monsters/{index_name}"
+    r = requests.get(url=url)
+    # extracting data in json format
+    data = r.json()
+    with open(f"monsters/{index_name}.json", "w") as f:
+        f.write(str(data))
 
 
 def populate_dungeon() -> List[str]:
@@ -40,7 +58,7 @@ def populate_dungeon() -> List[str]:
     return monster_list
 
 
-def request_monster(index_name: str) -> Monster:
+def request_monster_api(index_name: str) -> Monster:
     """
     Send a request to the DnD API for a monster's characteristic
     :param index_name: name of the monster
@@ -51,18 +69,41 @@ def request_monster(index_name: str) -> Monster:
     r = requests.get(url=url)
     # extracting data in json format
     data = r.json()
-    return Monster(name=data['name'], armor_class=data['armor_class'], hit_points=data['hit_points'], hit_dice=data['hit_dice'], xp=data['xp'])
+    return Monster(name=data['name'],
+                   armor_class=data['armor_class'],
+                   hit_points=data['hit_points'],
+                   hit_dice=data['hit_dice'],
+                   xp=data['xp'],
+                   challenge_rating=data['challenge_rating'])
+
+
+def request_monster(index_name: str) -> Monster:
+    """
+    Send a request to local database for a monster's characteristic
+    :param index_name: name of the monster
+    :return: Monster object
+    """
+    with open(f"monsters/{index_name}.json", "r") as f:
+        data = json.loads(f.read())
+    return Monster(name=data['name'],
+                   armor_class=data['armor_class'],
+                   hit_points=data['hit_points'],
+                   hit_dice=data['hit_dice'],
+                   xp=data['xp'],
+                   challenge_rating=data['challenge_rating'])
 
 
 if __name__ == '__main__':
-    character: Character = Character(name='philRG', armor_class=10, hit_points=100)
-    monsters: List[str] = populate_dungeon()
+    character: Character = Character(name='philRG', armor_class=10, hit_points=500, xp=0)
+    monsters_names: List[str] = populate_dungeon()
+    roster: List[Monster] = [request_monster(name) for name in monsters_names]
+    monsters_to_fight = [m for m in roster if m.challenge_rating > 20]
     attack_count = 0
     while character.hit_points > 0:
-        monster_name = random.choice(monsters)
-        monster: Monster = request_monster(index_name=monster_name)
+        monster = random.choice(monsters_to_fight)
         hp_damage = monster.attack()
         character.hit_points -= hp_damage
+        character.xp += monster.xp
         print(f'{monster.name} attacks {character.name} and {character.name} takes {hp_damage} hp')
         attack_count += 1
     print(f'{character} has been finally killed by a {monster.name} after {attack_count} attacks')
