5b95ccf826bfc1adf2576c0ee4a11fee0b7a7fdf
Philippe Mourey <philippe@mourey.com>
2022-12-05 10:51:10 +0100

    essais avec SQL Alchemy

---
+++ tools/common.py
@@ -0,0 +1,23 @@
+""" Needs to separate presentation layer from data layer """
+from enum import Enum
+
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+class AbilityType(Enum):
+    STR = 'str'
+    CON = 'con'
+    DEX = 'dex'
+    INT = 'int'
+    WIS = 'wis'
+    CHA = 'cha'
