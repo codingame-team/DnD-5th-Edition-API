421f420cc4b6d51692dae57141a3dc2d1b543f21
Philippe Mourey <philippe@mourey.com>
2023-01-08 17:11:26 +0100

    add get_key()

--- tools/common.py
+++ tools/common.py
@@ -1,4 +1,8 @@
 """ Needs to separate presentation layer from data layer """
+import os
+import sys
+import termios
+import tty
 from enum import Enum
 
 
@@ -28,3 +32,28 @@ def cprint(*args):
     # return
     # print(*args, file=sys.stderr, flush=True)
     print(*args, flush=True)
+
+def getkey():
+    old_settings = termios.tcgetattr(sys.stdin)
+    tty.setcbreak(sys.stdin.fileno())
+    try:
+        while True:
+            b = os.read(sys.stdin.fileno(), 3).decode()
+            if len(b) == 3:
+                k = ord(b[2])
+            else:
+                k = ord(b)
+            key_mapping = {
+                127: 'backspace',
+                10: 'return',
+                32: 'space',
+                9: 'tab',
+                27: 'esc',
+                65: 'up',
+                66: 'down',
+                67: 'right',
+                68: 'left'
+            }
+            return key_mapping.get(k, chr(k))
+    finally:
+        termios.tcsetattr(sys.stdin, termios.TCSADRAIN, old_settings)
---
+++ tools/tools.md
@@ -0,0 +1,2 @@
+
+- [How to detect key presses](https://stackoverflow.com/questions/24072790/how-to-detect-key-presses)
