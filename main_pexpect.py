1dcaf742f899466d9ed367a97f867e9f3b9528b8
Philippe Mourey <philippe@mourey.com>
2023-01-14 13:47:09 +0100

    added some testing for usage of main.py inside the IDE (emulating a Terminal using pexpect)

--- main_pexpect.py
+++ main_pexpect.py
@@ -16,6 +16,8 @@ actual_version = version_tuple(sys.version)
 # print(sys.version)
 # print(version_tuple(sys.version))
 # print(version_tuple(required_version))
+
+
 if actual_version < required_version:
     print(f'requires Python version at least 3.10 to run!')
     exit(1)
@@ -23,3 +25,4 @@ else:
     print(f'Python version {sys.version.split()[0]} is compatible!')
     python_version = '.'.join(map(str, actual_version[:2]))
     pty.spawn([f"python{python_version}", 'main.py'])
+
