f30209bbf428c3b002db7ef117c1f52257999dae
Philippe Mourey <philippe@mourey.com>
2024-06-27 01:41:51 +0200

    update lee algorithm to add a_star search (faster)

--- algo/lee.py
+++ algo/lee.py
@@ -19,6 +19,51 @@ from typing import List
 # Liste des déplacements possibles
 directions: List[tuple] = [(-1, 0), (0, -1), (1, 0), (0, 1)]
 
+from typing import List, Tuple
+from collections import deque
+import heapq
+
+def heuristic(x1: int, y1: int, x2: int, y2: int) -> int:
+    """
+    Heuristic function to estimate the cost from (x1, y1) to (x2, y2).
+    In this case, we use the Manhattan distance.
+    """
+    return abs(x1 - x2) + abs(y1 - y2)
+
+def parcours_a_star(carte: List[List[int]], start_x: int, start_y: int, end_x: int, end_y: int, obstacles: List[tuple] = None) -> Tuple[int, dict]:
+    width, height = len(carte[0]), len(carte)
+    visited = [[False] * width for _ in range(height)]
+    to_visit: list = [(0, start_x, start_y, 0)]  # (f_cost, x, y, g_cost)
+    min_dist = float('inf')
+    pred: dict = {}
+
+    if obstacles is None:
+        obstacles = []
+
+    while to_visit:
+        _, x, y, g_cost = heapq.heappop(to_visit)
+        if (x, y) == (end_x, end_y):
+            min_dist = g_cost
+            break
+
+        if not visited[y][x]:
+            visited[y][x] = True
+
+            for dx, dy in directions:
+                neigh_x, neigh_y = x + dx, y + dy
+                if 0 <= neigh_x < width and 0 <= neigh_y < height:
+                    # si le voisin est accessible
+                    if (neigh_x, neigh_y) not in obstacles and carte[neigh_y][neigh_x] == 1:
+                        if not visited[neigh_y][neigh_x]:
+                            # f_cost = g_cost + h_cost
+                            # g_cost is the actual cost of the path from the start node to the current node.
+                            # h_cost is the estimated cost from the current node to the goal node.
+                            f_cost = g_cost + 1 + abs(end_x - neigh_x) + abs(end_y - neigh_y)
+                            heapq.heappush(to_visit, (f_cost, neigh_x, neigh_y, g_cost + 1))
+                            pred[(neigh_x, neigh_y)] = x, y
+
+    return min_dist, pred
+
 
 # Retrouver la distance minimum entre le point (i, j) et la destination (x, y)
 def parcours_largeur(carte: List[List[int]], start_x: int, start_y: int, end_x: int, end_y: int, obstacles: List[tuple] = None) -> int:
