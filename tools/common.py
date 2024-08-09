ee1e1bd8638c31d8715e6839930c4ce362702587
Philippe Mourey <philippe@mourey.com>
2024-08-10 01:19:04 +0200

    add resource_path method for binary usage

--- tools/common.py
+++ tools/common.py
@@ -49,6 +49,11 @@ class AbilityType(Enum):
     WIS = 'wis'
     CHA = 'cha'
 
+def resource_path(relative_path):
+    """ Get the absolute path to a resource, works for dev and for PyInstaller """
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
 
 def cprint(*args):
     # return
