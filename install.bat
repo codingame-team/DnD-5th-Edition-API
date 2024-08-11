ab08164e60ae4dcff6c2e1cf26816c47ed261f5d
Philippe Mourey <philippe@mourey.com>
2024-08-11 12:25:51 +0200

    ajout script d'installation pour Windows (non testé)

---
+++ install.bat
@@ -0,0 +1,20 @@
+@echo off
+
+rem Check if PyInstaller is installed
+where /q pyinstaller
+if %errorlevel% neq 0 (
+    echo PyInstaller is not installed. Please install it first.
+    pause
+    exit /b
+)
+
+rem Create the executable
+pyinstaller --onefile ^
+    --add-data "collections;collections" ^
+    --add-data "data;data" ^
+    --add-data "sprites;sprites" ^
+    --add-data "images;images" ^
+    --add-data "maze;maze" ^
+    --add-data "Tables;Tables" ^
+    --add-data "gameState;gameState" ^
+    dungeon_menu_pygame.py
