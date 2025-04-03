86f03ea504dec8ae64bac5c753e9242bc4c14aee
Philippe Mourey <philippe.mourey@gmail.com>
2025-04-04 01:57:42 +0200

    add update buttons status function

--- pyQTApp/common.py
+++ pyQTApp/common.py
@@ -5,6 +5,7 @@ from enum import Enum
 from typing import List
 
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtWidgets import QPushButton, QFrame
 
 from dao_classes import Character
 
@@ -13,14 +14,20 @@ def debug(*args):
     # return
     print(*args, file=sys.stderr, flush=True)
 
+
 def load_welcome() -> QPixmap:
     path = os.path.dirname(__file__)
-    image_file: str = f'{path}/images/welcome.png'
+    image_file: str = f"{path}/images/welcome.png"
     pixmap = QPixmap(image_file)
     return pixmap
+
 
 class color:
     RED = '<span style="color: red">'
     GREEN = '<span style="color: green">'
-    END = '</span>'
+    END = "</span>"
 
+
+def update_buttons(frame: QFrame, enabled: bool):
+    for widget in frame.findChildren(QPushButton):
+        widget.setEnabled(enabled)
